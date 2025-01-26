ba9700951b2799ac7064393c47beff66be03928d
Ikosy <lendres@fifthrace.com>
2025-01-26 09:42:37 -0600

    Update TestArguments1.cs

--- src/Unit Tests/TestArguments1.cs
+++ src/Unit Tests/TestArguments1.cs
@@ -4,19 +4,23 @@ namespace UnitTests;
 
 public class TestArguments1
 {
+	QuotationInfo _quotationInfo = new('\"');
+			
+	public TestArguments1()
+	{
+		_quotationInfo.AddEscapeCode('\"', '\"');
+	}
+
 	[Fact]
-	public void Test1()
+	public void TestRawArguments()
 	{
 		Arguments1 arguments = new();
 		try
 		{
 			CommandLineParser parser = new(arguments);
 
-			string[] rawArguments = ["-filename", "\"Test File.txt\"", "-run"];
-			QuotationInfo quotationInfo = new('\"');
-			quotationInfo.AddEscapeCode('\"', '\"');
-			parser.AddQuotation(quotationInfo);
-			parser.Parse(rawArguments, false);
+			string[] input = ["-filename", "\"Test File.txt\"", "-run"];
+			parser.Parse(input, false);
 		}
 		catch (Exception exception)
 		{
@@ -24,6 +28,57 @@ public class TestArguments1
 			System.Diagnostics.Debug.WriteLine(exception.ToString());
 		}
 
+		Assert.Equal("Test File.txt", arguments.FileName);
+		Assert.True(arguments.Run);
+	}
+
+	[Fact]
+	public void TestRawWithExecutable()
+	{
+		Arguments1 arguments = new();
+		CommandLineParser? parser = null;
+		try
+		{
+			parser = new(arguments);
+
+			string[] input = ["\"C:\\Temp\\Example.exe\"", "-filename", "\"Test File.txt\"", "-run"];
+			//parser.AddQuotation(_quotationInfo);
+			parser.Parse(input, true);
+			
+		}
+		catch (Exception exception)
+		{
+			System.Diagnostics.Debug.WriteLine("");
+			System.Diagnostics.Debug.WriteLine(exception.ToString());
+		}
+
+		Assert.NotNull(parser);
+		Assert.Equal(@"C:\Temp\Example.exe", parser.ExecutablePath);
+		Assert.Equal("Test File.txt", arguments.FileName);
+		Assert.True(arguments.Run);
+	}
+
+	[Fact]
+	public void TestStringExecutable()
+	{
+		Arguments1 arguments = new();
+		CommandLineParser? parser = null;
+		try
+		{
+			parser = new(arguments);
+
+			string input = "\"C:\\Temp\\Example.exe\" -filename \"Test File.txt\" -run";
+			parser.Parse(input, true);
+			
+		}
+		catch (Exception exception)
+		{
+			System.Diagnostics.Debug.WriteLine("");
+			System.Diagnostics.Debug.WriteLine(exception.ToString());
+		}
+
+		Assert.NotNull(parser);
+		Assert.Equal(@"C:\Temp\Example.exe", parser.ExecutablePath);
 		Assert.Equal("Test File.txt", arguments.FileName);
 		Assert.True(arguments.Run);
 	}
