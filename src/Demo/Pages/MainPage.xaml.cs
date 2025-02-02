6c484982a4e16bb9b2d5687ec9ed04158e8ca5d1
Ikosy <lendres@fifthrace.com>
2025-02-02 09:31:37 -0600

    Organize demo

---
+++ src/Demo/Pages/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace UnlockingFlyout;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnNavigateForward(object sender, EventArgs e)
+	{
+		Shell.Current.GoToAsync(nameof(SecondPage), true, new Dictionary<string, object> { });
+	}
+}
---
+++ src/Demo/Program/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace UnlockingFlyout;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		Routing.RegisterRoute(nameof(SecondPage), typeof(SecondPage));
+	}
+}
