6a70ee97e694cf456bef105b1bd994e3978c931c
Ikosy <lendres@fifthrace.com>
2025-02-03 09:36:16 -0600

    Add char, int tests

--- src/Unit Tests/NullableManagers.cs
+++ src/Unit Tests/NullableManagers.cs
@@ -29,3 +29,56 @@ class NullableBoolManager
 	[CommandLineOption(Name = "run", BoolFunction = BoolFunction.TrueIfPresent, Description = "If true, the application will automatically start running.")]
 	public bool? Run { get; set; } = null;
 }
+
+
+[CommandLineManager(ApplicationName = "Nullable Test", Copyright = "Copyright (c) Lance A. Endres")]
+class ByteManager
+{
+	[CommandLineOption(Name = "value", Description = "Specifies the test value.")]
+	public byte Value { get; set; } = 0x00;
+}
+
+[CommandLineManager(ApplicationName = "Nullable Test", Copyright = "Copyright (c) Lance A. Endres")]
+class NullableByteManager
+{
+	[CommandLineOption(Name = "value", Description = "Specifies the test value.")]
+	public byte? Value { get; set; } = null;
+}
+
+[CommandLineManager(ApplicationName = "Nullable Test", Copyright = "Copyright (c) Lance A. Endres")]
+class CharManager
+{
+	[CommandLineOption(Name = "value", Description = "Specifies the test value.")]
+	public char Value { get; set; } = ' ';
+}
+
+[CommandLineManager(ApplicationName = "Nullable Test", Copyright = "Copyright (c) Lance A. Endres")]
+class NullableCharManager
+{
+	[CommandLineOption(Name = "value", Description = "Specifies the test value.")]
+	public char? Value { get; set; } = null;
+}
+
+[CommandLineManager(ApplicationName = "Nullable Test", Copyright = "Copyright (c) Lance A. Endres")]
+class IntManager
+{
+	[CommandLineOption(Name = "value", Description = "Specifies the test value.")]
+	public int Value { get; set; } = ' ';
+}
+
+[CommandLineManager(ApplicationName = "Nullable Test", Copyright = "Copyright (c) Lance A. Endres")]
+class NullableIntManager
+{
+	[CommandLineOption(Name = "value", Description = "Specifies the test value.")]
+	public int? Value { get; set; } = null;
+}
+
+//sbyte
+//decimal
+//double
+//float
+//uint
+//long
+//ulong
+//short
+//ushort
--- src/Unit Tests/TestNullableArguments.cs
+++ src/Unit Tests/TestNullableArguments.cs
@@ -11,7 +11,7 @@ public class TestNullableArguments
 		_quotationInfo.AddEscapeCode('\"', '\"');
 	}
 
-	private T GetArgumentsInstance<T>(string commandLineString) where T : class, new()
+	private static T GetArgumentsInstance<T>(string commandLineString) where T : class, new()
 	{
 		T nullableArguments	= new();
 		try
@@ -26,6 +26,24 @@ public class TestNullableArguments
 			System.Diagnostics.Debug.WriteLine(exception.ToString());
 		}
 		return nullableArguments;
+	}
+
+	private static CommandLineParser GetParser<T>(string commandLineString) where T : class, new()
+	{
+		T nullableArguments	= new();
+		try
+		{
+			CommandLineParser parser = new(nullableArguments);
+			Assert.NotNull(parser);
+			parser.Parse(commandLineString, false);
+			return parser;
+		}
+		catch (Exception exception)
+		{
+			System.Diagnostics.Debug.WriteLine("");
+			System.Diagnostics.Debug.WriteLine(exception.ToString());
+			throw;
+		}
 	}
 
 	[Fact]
@@ -60,5 +78,59 @@ public class TestNullableArguments
 
 		arguments = GetArgumentsInstance<NullableBoolManager>("");
 		Assert.Null(arguments.Run);
+	}
+
+	[Fact]
+	public void TestByte()
+	{
+		ByteManager arguments = GetArgumentsInstance<ByteManager>("-value 0x32");
+		Assert.Equal(0x32, arguments.Value);
+	}
+
+	[Fact]
+	public void TestNullableByte()
+	{
+		NullableByteManager arguments = GetArgumentsInstance<NullableByteManager>("-value 0x32");
+		Assert.Equal((byte)0x32, arguments.Value);
+
+		arguments = GetArgumentsInstance<NullableByteManager>("");
+		Assert.Null(arguments.Value);
+	}
+
+	[Fact]
+	public void TestChar()
+	{
+		CharManager arguments = GetArgumentsInstance<CharManager>("-value x");
+		Assert.Equal('x', arguments.Value);
+
+		CommandLineParser parser = GetParser<CharManager>("-value xx");
+		Assert.Equal("The value \"xx\" is not valid for option \"value\"; Character options must be a single character.", parser.Errors.First().Message);
+	}
+
+	[Fact]
+	public void TestNullableChar()
+	{
+		NullableCharManager arguments = GetArgumentsInstance<NullableCharManager>("-value x");
+		Assert.Equal('x', arguments.Value);
+
+		arguments = GetArgumentsInstance<NullableCharManager>("");
+		Assert.Null(arguments.Value);
+	}
+
+	[Fact]
+	public void TestInt()
+	{
+		IntManager arguments = GetArgumentsInstance<IntManager>("-value 77");
+		Assert.Equal(77, arguments.Value);
+	}
+
+	[Fact]
+	public void TestNullableInt()
+	{
+		NullableIntManager arguments = GetArgumentsInstance<NullableIntManager>("-value 77");
+		Assert.Equal(77, arguments.Value);
+
+		arguments = GetArgumentsInstance<NullableIntManager>("");
+		Assert.Null(arguments.Value);
 	}
 }
