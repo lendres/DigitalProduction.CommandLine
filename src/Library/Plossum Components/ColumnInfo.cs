98ddf072f45e60d3e10600c2d923373a78ab2964
Ikosy <lendres@fifthrace.com>
2025-02-04 07:16:41 -0600

    Simplify licensing
    Simplify the licensing by including one license file for the entire project and removing the individual licensing strings at the top of the files.  This maintains the terms of the copyright and removes over 1200 lines of redundant comments.

--- src/Library/Plossum Components/ColumnInfo.cs
+++ src/Library/Plossum Components/ColumnInfo.cs
@@ -1,185 +1,155 @@
-/* Copyright (c) Peter Palotas 2007
- *  
- *  All rights reserved.
- *  
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are
- *  met:
- *  
- *      * Redistributions of source code must retain the above copyright 
- *        notice, this list of conditions and the following disclaimer.    
- *      * Redistributions in binary form must reproduce the above copyright 
- *        notice, this list of conditions and the following disclaimer in 
- *        the documentation and/or other materials provided with the distribution.
- *      * Neither the name of the copyright holder nor the names of its 
- *        contributors may be used to endorse or promote products derived 
- *        from this software without specific prior written permission.
- *  
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-namespace DigitalProduction.CommandLine
+namespace DigitalProduction.CommandLine;
+
+/// <summary>
+/// Represents a column to be used with <see cref="StringFormatter.FormatInColumns"/>.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="ColumnInfo"/> struct.
+/// </remarks>
+/// <param name="width">The width of the column in (fixed-width) characters.</param>
+/// <param name="content">The content of this column.</param>
+/// <param name="alignment">The alignment to use for this column.</param>
+/// <param name="verticalAlignment">The vertical alignment to use for this column</param>        
+/// <param name="method">The word wrapping method to use for this column</param>
+public struct ColumnInfo(int width, string content, Alignment alignment, VerticalAlignment verticalAlignment, WordWrappingMethod method)
 {
+	#region Private Fields
+
+	private readonly WordWrappingMethod mWordWrappingMethod     = method;
+	private readonly Alignment          mAlignment              = alignment;
+	private readonly int                mWidth                  = width;
+	private readonly string             mContent                = content;
+	private VerticalAlignment           mVerticalAlignment      = verticalAlignment;
+
+	#endregion
+
+	#region Constructors
+
 	/// <summary>
-	/// Represents a column to be used with <see cref="StringFormatter.FormatInColumns"/>.
-	/// </summary>
-	/// <remarks>
 	/// Initializes a new instance of the <see cref="ColumnInfo"/> struct.
-	/// </remarks>
+	/// </summary>
 	/// <param name="width">The width of the column in (fixed-width) characters.</param>
 	/// <param name="content">The content of this column.</param>
 	/// <param name="alignment">The alignment to use for this column.</param>
-	/// <param name="verticalAlignment">The vertical alignment to use for this column</param>        
-	/// <param name="method">The word wrapping method to use for this column</param>
-	public struct ColumnInfo(int width, string content, Alignment alignment, VerticalAlignment verticalAlignment, WordWrappingMethod method)
+	/// <param name="verticalAlignment">The vertical alignment to use for this column</param>
+	public ColumnInfo(int width, string content, Alignment alignment, VerticalAlignment verticalAlignment)
+			: this(width, content, alignment, verticalAlignment, WordWrappingMethod.Optimal)
 	{
-        #region Private Fields
+	}
 
-        private readonly WordWrappingMethod	mWordWrappingMethod		= method;
-        private readonly Alignment			mAlignment				= alignment;
-        private readonly int				mWidth					= width;
-        private readonly string				mContent				= content;
-        private VerticalAlignment			mVerticalAlignment		= verticalAlignment;
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ColumnInfo"/> struct.
+	/// </summary>
+	/// <param name="width">The width of the column in (fixed-width) characters.</param>
+	/// <param name="content">The content of this column.</param>
+	/// <param name="alignment">The alignment to use for this column.</param>
+	/// <remarks>The word wrapping method used will be the one described by <see cref="Plossum.WordWrappingMethod.Optimal"/>.</remarks>
+	public ColumnInfo(int width, string content, Alignment alignment)
+		: this(width, content, alignment, VerticalAlignment.Top)
+	{
+	}
 
-        #endregion
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ColumnInfo"/> struct.
+	/// </summary>
+	/// <param name="width">The width of the column in (fixed-width) characters.</param>
+	/// <param name="content">The content of this column.</param>
+	/// <remarks>The word wrapping method used will be the one described by <see cref="Plossum.WordWrappingMethod.Optimal"/>, and 
+	/// each line in this column will be left aligned.</remarks>
+	public ColumnInfo(int width, string content)
+		: this(width, content, Alignment.Left)
+	{
+	}
 
-		#region Constructors
+	#endregion
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="ColumnInfo"/> struct.
-		/// </summary>
-		/// <param name="width">The width of the column in (fixed-width) characters.</param>
-		/// <param name="content">The content of this column.</param>
-		/// <param name="alignment">The alignment to use for this column.</param>
-		/// <param name="verticalAlignment">The vertical alignment to use for this column</param>
-		public ColumnInfo(int width, string content, Alignment alignment, VerticalAlignment verticalAlignment)
-            : this(width, content, alignment, verticalAlignment, WordWrappingMethod.Optimal)
-        {
-        }
+	#region Public Methods
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ColumnInfo"/> struct.
-        /// </summary>
-        /// <param name="width">The width of the column in (fixed-width) characters.</param>
-        /// <param name="content">The content of this column.</param>
-        /// <param name="alignment">The alignment to use for this column.</param>
-        /// <remarks>The word wrapping method used will be the one described by <see cref="Plossum.WordWrappingMethod.Optimal"/>.</remarks>
-        public ColumnInfo(int width, string content, Alignment alignment)
-            : this(width, content, alignment, VerticalAlignment.Top)
-        {
-        }
+	/// <summary>
+	/// Implements the operator ==.
+	/// </summary>
+	/// <param name="lhs">The LHS.</param>
+	/// <param name="rhs">The RHS.</param>
+	/// <returns>The result of the operator.</returns>
+	public static bool operator ==(ColumnInfo lhs, ColumnInfo rhs)
+	{
+		return lhs.Equals(rhs);
+	}
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ColumnInfo"/> struct.
-        /// </summary>
-        /// <param name="width">The width of the column in (fixed-width) characters.</param>
-        /// <param name="content">The content of this column.</param>
-        /// <remarks>The word wrapping method used will be the one described by <see cref="Plossum.WordWrappingMethod.Optimal"/>, and 
-        /// each line in this column will be left aligned.</remarks>
-        public ColumnInfo(int width, string content)
-            : this(width, content, Alignment.Left)
-        {
-        }
+	/// <summary>
+	/// Implements the operator !=.
+	/// </summary>
+	/// <param name="lhs">The LHS.</param>
+	/// <param name="rhs">The RHS.</param>
+	/// <returns>The result of the operator.</returns>
+	public static bool operator !=(ColumnInfo lhs, ColumnInfo rhs)
+	{
+		return !lhs.Equals(rhs);
+	}
 
-        #endregion
+	/// <summary>
+	/// Indicates whether this instance and a specified object are equal.
+	/// </summary>
+	/// <param name="obj">Another object to compare to.</param>
+	/// <returns>
+	/// true if obj and this instance are the same type and represent the same value; otherwise, false.
+	/// </returns>
+	public override readonly bool Equals(object? obj)
+	{
+		if (obj is not ColumnInfo)
+		{
+			return false;
+		}
 
-        #region Public Methods
+		ColumnInfo ci = (ColumnInfo)obj;
 
-        /// <summary>
-        /// Implements the operator ==.
-        /// </summary>
-        /// <param name="lhs">The LHS.</param>
-        /// <param name="rhs">The RHS.</param>
-        /// <returns>The result of the operator.</returns>
-        public static bool operator ==(ColumnInfo lhs, ColumnInfo rhs)
-        {
-            return lhs.Equals(rhs);
-        }
+		return Width.Equals(ci.Width) && Content.Equals(ci.Content) && Alignment.Equals(ci.Alignment) && WordWrappingMethod.Equals(ci.WordWrappingMethod);
+	}
 
-        /// <summary>
-        /// Implements the operator !=.
-        /// </summary>
-        /// <param name="lhs">The LHS.</param>
-        /// <param name="rhs">The RHS.</param>
-        /// <returns>The result of the operator.</returns>
-        public static bool operator !=(ColumnInfo lhs, ColumnInfo rhs)
-        {
-            return !lhs.Equals(rhs);
-        }
+	/// <summary>
+	/// Returns the hash code for this instance.
+	/// </summary>
+	/// <returns>
+	/// A 32-bit signed integer that is the hash code for this instance.
+	/// </returns>
+	public override readonly int GetHashCode()
+	{
+		return Width.GetHashCode() ^ Content.GetHashCode() ^ Alignment.GetHashCode() ^ WordWrappingMethod.GetHashCode();
+	}
 
-        /// <summary>
-        /// Indicates whether this instance and a specified object are equal.
-        /// </summary>
-        /// <param name="obj">Another object to compare to.</param>
-        /// <returns>
-        /// true if obj and this instance are the same type and represent the same value; otherwise, false.
-        /// </returns>
-        public override readonly bool Equals(object? obj)
-        {
-			if (obj is not ColumnInfo)
-			{
-				return false;
-			}
+	#endregion
 
-            ColumnInfo ci = (ColumnInfo)obj;
+	#region Public Properties 
 
-            return Width.Equals(ci.Width) && Content.Equals(ci.Content) && Alignment.Equals(ci.Alignment) && WordWrappingMethod.Equals(ci.WordWrappingMethod);
-        }
+	/// <summary>
+	/// Gets the width of this column in fixed width characters.
+	/// </summary>
+	/// <value>the width of this column in fixed width characters.</value>
+	public readonly int Width { get => mWidth; }
 
-        /// <summary>
-        /// Returns the hash code for this instance.
-        /// </summary>
-        /// <returns>
-        /// A 32-bit signed integer that is the hash code for this instance.
-        /// </returns>
-        public override readonly int GetHashCode()
-        {
-            return Width.GetHashCode() ^ Content.GetHashCode() ^ Alignment.GetHashCode() ^ WordWrappingMethod.GetHashCode();
-        }
+	/// <summary>
+	/// Gets the content of this column.
+	/// </summary>
+	/// <value>The content of this column.</value>
+	public readonly string Content { get => mContent; }
 
-        #endregion
+	/// <summary>
+	/// Gets the alignment of this column.
+	/// </summary>
+	/// <value>The alignment of this column.</value>
+	public readonly Alignment Alignment { get => mAlignment; }
 
-        #region Public Properties 
+	/// <summary>
+	/// Gets the word wrapping method to use for this column.
+	/// </summary>
+	/// <value>The the word wrapping method to use for this column.</value>
+	public readonly WordWrappingMethod WordWrappingMethod { get => mWordWrappingMethod; }
 
-        /// <summary>
-        /// Gets the width of this column in fixed width characters.
-        /// </summary>
-        /// <value>the width of this column in fixed width characters.</value>
-        public readonly int Width { get => mWidth; }
+	/// <summary>
+	/// Gets or sets the vertical alignment of the contents of this column.
+	/// </summary>
+	/// <value>The vertical alignment of this column.</value>
+	public VerticalAlignment VerticalAlignment { readonly get => mVerticalAlignment; set => mVerticalAlignment = value; }
 
-        /// <summary>
-        /// Gets the content of this column.
-        /// </summary>
-        /// <value>The content of this column.</value>
-        public readonly string Content { get => mContent; }
-
-        /// <summary>
-        /// Gets the alignment of this column.
-        /// </summary>
-        /// <value>The alignment of this column.</value>
-        public readonly Alignment Alignment { get => mAlignment; }
-
-        /// <summary>
-        /// Gets the word wrapping method to use for this column.
-        /// </summary>
-        /// <value>The the word wrapping method to use for this column.</value>
-        public readonly WordWrappingMethod WordWrappingMethod { get => mWordWrappingMethod; }
-
-        /// <summary>
-        /// Gets or sets the vertical alignment of the contents of this column.
-        /// </summary>
-        /// <value>The vertical alignment of this column.</value>
-        public VerticalAlignment VerticalAlignment { readonly get => mVerticalAlignment; set => mVerticalAlignment = value; }
-	
-        #endregion
-    }
+	#endregion
 }
